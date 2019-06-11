3ec797760f264e6e2dbe7e688e281bab6ae0f17f
Swarnim Arun <swarnimarun11@gmail.com>
2019-06-12 03:29:55 +0530

    Update Scripts

--- py-code/lookup.py
+++ py-code/lookup.py
@@ -1,15 +1,16 @@
 
 import string
 import random
+import string_sum
 
 def unique_count(val):
-    ls = []
-    for x in val:
-        if not x in ls:
-            ls.append(x)
-    print(len(ls))
+    ls = set(list(val))
+    return len(ls)
 
 val = ''.join(random.choice(string.printable) for i in range(1000000))
 
 def test_pure_python(benchmark):
     benchmark(unique_count, val)
+
+def test_mod_rust(benchmark):
+    benchmark(string_sum.unique_count, val)
